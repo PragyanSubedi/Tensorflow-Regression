87fb0e53278449bb0310576b1965ef2893c7c0ed
PragyanSubedi <pragyansubedi@gmail.com>
2018-02-10 08:50:04 +0545

    Debugged errors

--- NN example.py
+++ NN example.py
@@ -7,9 +7,9 @@ n_features = 10
 n_dense_neurons = 3
 
 x = tf.placeholder(tf.float32,(None,n_features))  #[anything,10]
-w= tf.variable(tf_random_normal([n_features,n_dense_neurons])) # [10,3]
+w= tf.Variable(tf.random_normal([n_features,n_dense_neurons])) # [10,3]
 
-b= tf. variable (tf.ones([n_dense_neurons])) # [1,1,1]
+b= tf. Variable (tf.ones([n_dense_neurons])) # [1,1,1]
 
 xW = tf.matmul(x,w) #[1, 10] *[10,3]
 z= tf.add(xW,b) # [1,3] + [1,3]
@@ -17,9 +17,9 @@ a = tf.sigmoid(z) #Activation function
 
 init = tf.global_variables_initializer()
 
-with tf.session() as sess:
+with tf.Session() as sess:
     sess.run(init)
-    layer_out = sess.run(a, feed_dict={x:np.random.random([1,n_feaures])})
+    layer_out = sess.run(a, feed_dict={x:np.random.random([1,n_features])})
 
 print(layer_out)
 
